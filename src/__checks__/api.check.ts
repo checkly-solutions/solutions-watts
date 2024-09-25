bc78ca03f35c84b528b7e0cd297c30800056220d
Austin Butcher <abutcher@wattsandassociates.com>
2024-09-25 16:07:53 -0600

    Add environment variables and some tests.

--- src/__checks__/api.check.ts
+++ src/__checks__/api.check.ts
@@ -65,8 +65,8 @@ new ApiCheck('get-mpci-counties', {
   runParallel: true,
 });
 
-new ApiCheck('get-mpci-commodoties', {
-  name: 'GET Mpci Commodoties',
+new ApiCheck('get-mpci-commodities', {
+  name: 'GET Mpci Commodities',
   group: uatGroup,
   tags: ['uat', 'commodities', 'mpci'],
   degradedResponseTime: 3000,
@@ -75,7 +75,7 @@ new ApiCheck('get-mpci-commodoties', {
     entrypoint: path.join(__dirname, './utils/setup.ts'),
   },
   request: {
-    url: 'https://mpciquoterapi-uat.wattsandassociates.com/api/4001/Lookups/GetMpciCommodities?ReinsuranceYear=2025&StateCode=1&CountyCode=1&IncludePriorYearData=false',
+    url: 'https://{{baseUrlQuoter}}/api/{{tenantID}}/Lookups/GetMpciCommodities',
     method: 'GET',
     // headers: [
     //   {
@@ -83,18 +83,239 @@ new ApiCheck('get-mpci-commodoties', {
     //     value: 'Bearer {{{WATTS_TOKEN}}}',
     //   },
     // ],
+    body: ``,
+    bodyType: 'NONE',
+    queryParameters: [
+      { key: 'ReinsuranceYear', value: '{{lookupReinsuranceYear}}' },
+      { key: 'StateCode', value: '1' },
+      { key: 'CountyCode', value: '1' },
+      { key: 'IncludePriorYearData', value: 'false' },
+    ],
+    followRedirects: true,
+    skipSSL: false,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200),
+      AssertionBuilder.jsonBody('$[0].id').isNotNull(),
+    ],
+  },
+  runParallel: true,
+});
+
+new ApiCheck('calculate-unit-quotes', {
+  name: 'POST Calculate Unit Quotes UAT',
+  group: uatGroup,
+  tags: ['uat', 'unitQuotes', 'mpci'],
+  degradedResponseTime: 3000,
+  maxResponseTime: 8000,
+  setupScript: {
+    entrypoint: path.join(__dirname, './utils/setup.ts'),
+  },
+  request: {
+    url: 'https://{{baseUrlQuoter}}/api/{{tenantID}}/MpciQuoting/CalculateUnitQuotes',
+    method: 'POST',
     body: `{
-    "ReinsuranceYear": 2023,
-    "StateCode": 6,
-    "CountyCode": 107,
-    "SalesClosingDate": "2023-03-15T00:00:00"
-    }`,
+    "Units": [
+     {
+        "UnitCorrelationID": "unit1",
+        "BasicUnitNumber": "0001",
+        "OptionalUnitNumber": "0001",
+        "StateCode": 12,
+        "CountyCode": 9,
+        "CommodityCode": 201,
+        "TypeCode": 101,
+        "PracticeCode": 997,
+        "ReinsuranceYear": 2023,
+        "CommodityYear": 2024,
+        "ReportedAcres": 200,
+        "InsuredSharePercent": 1,
+        "GuaranteeAdjustmentFactor": 1,
+        "ExperienceFactor": 1,
+        "RangeClassCode": "D02",
+        "AcreageInsuranceOptionCodes": [
+        
+        ],
+        "InsInForceInsuranceOptionCodes": [
+          ]
+      }
+    ],
+    "QuoteElections": [
+      {
+        "ReinsuranceYear": 2023,
+        "InsurancePlanCode": 50,
+        "CoverageLevel": 0.85,
+        "UnitStructureCode": "OU",
+        "CoverageTypeCode": "A",
+        "PriceElectionPercent": 1,
+        "BaseInsuranceOptionCodes": [],
+        "ToggleInsuranceOptionCodes": [
+        ],
+        "AssociatedPlanParameters": [
+        ],
+        "AssociatedUnitCorrelationIDs": [
+        ]
+      }
+    ],
+    "PriceElections": [
+      {
+        "Price": {
+          "PriceCorrelationID": "noPrice",
+          "ProjectedPrice": 1,
+          "VolatilityFactor": 0
+        },
+        "AssociatedUnitCorrelationIDs": [
+        ]
+      }
+    ]
+  }`,
     bodyType: 'JSON',
     followRedirects: true,
     skipSSL: false,
     assertions: [
       AssertionBuilder.statusCode().equals(200),
       AssertionBuilder.jsonBody('$[0].id').isNotNull(),
+    ],
+  },
+  runParallel: true,
+});
+
+new ApiCheck('calculate-backcasts', {
+  name: 'POST Calculate Backcasts',
+  group: uatGroup,
+  tags: ['uat', 'backcasts', 'mpci'],
+  degradedResponseTime: 3000,
+  maxResponseTime: 8000,
+  setupScript: {
+    entrypoint: path.join(__dirname, './utils/setup.ts'),
+  },
+  request: {
+    url: 'https://{{baseUrlQuoter}}/api/{{tenantID}}/Analytics/CalculateBackcasts',
+    method: 'POST',
+    body: `{
+	"Units": [
+		{
+			"UnitCorrelationID": "unit1",
+			"BasicUnitNumber": "0001",
+			"OptionalUnitNumber": "0001",
+			"StateCode": 17,
+			"CountyCode": 1,
+			"CommodityCode": 41,
+			"TypeCode": 16,
+			"PracticeCode": 3,
+			"ReinsuranceYear": 2023,
+			"CommodityYear": 2023,
+			"ReportedAcres": 2000,
+			"InsuredSharePercent": 1,
+			"ApprovedYield": 247,
+			"RateYield": 207,
+			"AdjustedYield": 221,
+			"AcreageInsuranceOptionCodes": [
+			"TA"
+			],
+			"InsInForceInsuranceOptionCodes": [
+			],
+			"HistoricYields":[
+				{
+					"YieldCommodityYear": 2013,
+					"YieldTypeCode": "A",
+					"AnnualYield": 100,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2014,
+					"YieldTypeCode": "A",
+					"AnnualYield": 100,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2015,
+					"YieldTypeCode": "A",
+					"AnnualYield": 100,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2016,
+					"YieldTypeCode": "A",
+					"AnnualYield": 100,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2017,
+					"YieldTypeCode": "A",
+					"AnnualYield": 100,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2018,
+					"YieldTypeCode": "A",
+					"AnnualYield": 100,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2019,
+					"YieldTypeCode": "A",
+					"AnnualYield": 300,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2020,
+					"YieldTypeCode": "A",
+					"AnnualYield": 300,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2021,
+					"YieldTypeCode": "A",
+					"AnnualYield": 300,
+					"YieldAcreage": 2000
+				},
+				{
+					"YieldCommodityYear": 2022,
+					"YieldTypeCode": "A",
+					"AnnualYield": 300,
+					"YieldAcreage": 2000
+				}
+	
+			]
+		}
+		],
+	"PlanElections": [
+		{
+		"PlanKey": {
+			"PlanCorrelationID": "RP",
+			"ReinsuranceYear": 2023,
+			"InsurancePlanCode": 2,
+			"CoverageLevel": 0.85,
+			"UnitStructureCode": "EU",
+			"CoverageTypeCode": "A",
+			"PriceElectionPercent": 1,
+			"ReplaceUnitOptions": false,
+			"InsuranceOptionCodes": [
+			],
+			"AssociatedPlanParameters": [
+			]
+		},
+		"AssociatedUnitCorrelationIDs": [
+		]
+		}
+	],
+	"PriceElections": [
+		{
+			"Price": {
+			"PriceCorrelationID": "2023CornPrice",
+			"ProjectedPrice": 5.91,
+			"VolatilityFactor": 0.18
+			},
+			"AssociatedUnitCorrelationIDs": [
+			]
+		}
+	]
+	}`,
+    bodyType: 'JSON',
+    followRedirects: true,
+    skipSSL: false,
+    // This endpoint is unauthorized for this tenant. Test is failing
+    assertions: [
+      AssertionBuilder.statusCode().equals(401)
     ],
   },
   runParallel: true,
