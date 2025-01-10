3e09c3e3071c70a7b743132e080f9d8c3fbbad16
jonathan <joncanales2@gmail.com>
2025-01-10 16:34:06 -0500

    all dev & production environments generating tokens

--- src/__checks__/environment-auth/dev/bic-quoter-develop.spec.ts
+++ src/__checks__/environment-auth/dev/bic-quoter-develop.spec.ts
@@ -1,27 +1,34 @@
 import { test, expect } from '@playwright/test';
+import { createChecklyContext } from '../../utils/checklyRequestContext';
+import { signIn, sessionHandler } from '../../utils/auth-client';
 
-test('test', async ({ page }) => {
-  test.setTimeout(60000)
-  await page.goto('https://bic-quoter-develop.azurewebsites.net/')
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.getByPlaceholder('Username').fill('checkly@brisk.ag');
-  await page.getByRole('button', { name: 'Sign In' }).click();
-  await page.waitForTimeout(2000)
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill(`${process.env.WATTS_CLIENT_PASS_MS}`);
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.waitForTimeout(2000)
-  
-  await page.getByRole('button', { name: 'Yes' }).click();
-  await page.waitForTimeout(3000)
-  
-  // await page.getByRole('button', { name: 'Next' }).click();
-  // await page.waitForTimeout(2000)
+const envName = 'Training DEV Bic Quoter';
+const envUrl = 'https://bic-quoter-develop.azurewebsites.net/';
+const envVariableName = 'DEV_TRAINING_TOKEN';
+const userPass = process.env.WATTS_CLIENT_PASS_MS || 'none';
+const apiKey = process.env.TOKEN_WRITER_API_KEY || 'none';
+const accountID = process.env.CHECKLY_ACCOUNT_ID || 'none';
 
+test(`${envName} token update`, async ({ page }) => {
+  // Create context for issuing storate state update request
+  const context = await createChecklyContext(apiKey, accountID);
 
-  // await page.getByRole('link', { name: 'Skip setup' }).click();
-  // await page.waitForTimeout(3000)
+  test.setTimeout(45000);
+  await page.goto(`${envUrl}`);
 
-  // await page.getByRole('button', { name: 'Yes' }).click();
-  await page.waitForTimeout(5000)
+  await signIn(page, userPass);
+
+  let accessToken = await sessionHandler(page);
+
+  // Store the token in context
+  if (accessToken) {
+    context.put(`variables/${envVariableName}`, {
+      data: {
+        key: `${envVariableName}`,
+        value: `${accessToken}`,
+      },
+    });
+  } else {
+    console.error('Access token is not available for context storage.');
+  }
 });
